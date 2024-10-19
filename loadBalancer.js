7340a03461b7eb3f64fc4cf4c690caa876b76373
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-10-19 03:42:39 -0500

    implement round robin

--- loadBalancer.js
+++ loadBalancer.js
@@ -5,7 +5,16 @@ import { logRequest } from './utils/utils.js';
 const app = express();
 const PORT = 80;
 
-const backendServer = 'http://localhost:8080';
+let index = 0;
+
+const servers = ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082'];
+
+const getNextServer = () => {
+    if (index >= servers.length) {
+        index = 0;
+    }
+    return servers[index++];
+}
 
 app.use((req, res, next) => {
     console.log(logRequest(req, 'load-balancer'));
@@ -13,8 +22,9 @@ app.use((req, res, next) => {
 });
 
 app.get('*', async (req, res) => {
+    const server = getNextServer();
     try {
-        const result = await axios.get(backendServer + req.url);
+        const result = await axios.get(server + req.url);
         res.status(result.status).send(result.data);
     } catch (error) {
         res.status(500).send('Failed to connect to backend');
--- servers/server3.js
+++ servers/server3.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import { logRequest } from '../utils/utils.js';
 
 const app = express();
 const PORT = 8082;
+
+app.use((req, res, next) => {
+    console.log(logRequest(req, 'server3'));
+    next();
+})
 
 app.get('/', (req, res) => {
     res.send('hello from server3');
